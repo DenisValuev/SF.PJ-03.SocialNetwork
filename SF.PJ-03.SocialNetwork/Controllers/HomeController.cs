9086de686b18d6d42fb0dcd6a1d6108b0499fba1
Denis_Valuev <valuev.denis91@mail.ru>
2025-04-23 16:49:45 +0300

    Добавьте файлы проекта.

---
+++ SF.PJ-03.SocialNetwork/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SF.PJ_03.SocialNetwork.Models;
+
+namespace SF.PJ_03.SocialNetwork.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ SF.PJ-03.SocialNetwork/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SF.PJ_03.SocialNetwork.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
