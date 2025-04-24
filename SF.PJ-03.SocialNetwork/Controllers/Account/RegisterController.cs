f35c3c94732e89e1f20f0c32ec9260cb5a274957
Denis_Valuev <valuev.denis91@mail.ru>
2025-04-24 11:20:01 +0300

    SF.PJ-03.SocialNetwork-dev: Добавлены представления Register и RegisterPart2, добавлен контроллер с двумя методами GET

---
+++ SF.PJ-03.SocialNetwork/Controllers/Account/RegisterController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SF.PJ_03.SocialNetwork.ViewModels.Account;
+
+namespace SF.PJ_03.SocialNetwork.Controllers.Account
+{
+    public class RegisterController : Controller
+    {
+        [Route("Register")]
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View("Home/Register");
+        }
+
+        [Route("RegisterPart2")]
+        [HttpGet]
+        public IActionResult RegisterPart2(RegisterViewModel model)
+        {
+            return View("RegisterPart2", model);
+        }
+    }
+}
