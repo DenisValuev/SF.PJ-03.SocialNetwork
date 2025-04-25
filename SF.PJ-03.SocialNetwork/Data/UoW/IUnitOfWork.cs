2e52bd803bde1269cbfa5042d19d5ccd34926359
Denis_Valuev <valuev.denis91@mail.ru>
2025-04-25 09:48:00 +0300

    SF.PJ-03.SocialNetwork-dev: Добавлены интерфейсы для работы с репозиториями

---
+++ SF.PJ-03.SocialNetwork/Data/UoW/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using SF.PJ_03.SocialNetwork.Data.Repository;
+
+namespace SF.PJ_03.SocialNetwork.Data.UoW
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        int SaveChahges(bool ensureAutoHistory = false);
+        IRepository<TEntity> GetRepository<TEntity>(bool hasCustomRepository = true) where TEntity : class;
+    }
+}
